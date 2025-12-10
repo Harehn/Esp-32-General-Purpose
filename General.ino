e820b8f0d2d9d89c6368f502478cc0f518b548ba
Nitin <harehn.kaundun@mail.mcgill.ca>
2025-12-09 21:54:38 -0500

    Made code clearer and added internal storage code

--- General.ino
+++ General.ino
@@ -12,17 +12,29 @@
 #define I2C_SCL 22
 
 void setup() {
+  //------------------- SET UP DRIVERS AND CODE ---------------------------
   Wire.begin(I2C_SDA, I2C_SDA);
   Serial.begin(115200);
   if(! I2C_check(VERBOSE)) Serial.println("Error in I2C.");
   if(! set_up_temperature()) Serial.println("Error in Setting temperature sensor.");
   if(! set_up_ledc()) Serial.println("Error in setting up ledc.");
+  if(! set_up_storage()) Serial.println("Storage didn't set up properly.")
+
+  //------------------- OTHER INITIALIZATIONS -----------------------------
+  set_count();
 }
  
 void loop() {
+  //-------------------- TEMPERATURE --------------------------------------
   float temp = get_temperature();
   if (temp == -55.0f) Serial.println("Temperature Reading Error");
   else Serial.printf("Internal Temperature is %f.", temp);
+
+  //-------------------- LEDC ---------------------------------------------
   set_ledc_duty(50); // 0 - 255
+  
+  //-------------------- COUNT UPDATE/INTERNAL STORAGE --------------------
+  increase_count();
+
   delay(5000);
 }
